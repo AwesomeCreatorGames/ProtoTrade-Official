1d919ad41915f305e3b8068dba82f3cfee2814e3
AwesomeCreatorGames <74663739+AwesomeCreatorGames@users.noreply.github.com>
2021-06-08 11:03:19 +0530

    Update script.js

--- ProtoTrade/Login Pages/Introduction Page/Main Page/script.js
+++ ProtoTrade/Login Pages/Introduction Page/Main Page/script.js
@@ -12,3 +12,16 @@ $('.close').on('click', function(){
     $('body').removeClass('notification-open');
   })
 })
+$(function() {
+  var chatWidget = (".chat-widget-container"),
+      chatBox = $(".chat-box-container");
+  
+  $(chatWidget).click(function(e){
+    
+    e.preventDefault();
+    
+    $(chatBox).toggleClass("show");
+    $(chatWidget).toggleClass("open");
+  })
+  
+});
